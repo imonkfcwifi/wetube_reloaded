e32736fcdb70baf98666b4d5db23da0ecb415f59
imonkfcwifi <nohyungkorea@gmail.com>
2022-09-28 17:16:14 +0900

    digit

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -4,9 +4,10 @@ import { watch, edit, deleteVideo, upload } from "../controllers/videoController
 const videoRouter = express.Router();
 
 videoRouter.get("/upload", upload);
-videoRouter.get("/:id", watch);
+videoRouter.get("/:id(\\d+)", watch);
 // upload를 id로 볼 수 있기때문에 id를 upload 밑에 놔야 함.
-videoRouter.get("/:id/edit", edit);
-videoRouter.get("/:id/delete", deleteVideo);
+// updated : (\\d+)을 사용함으로 인해 digit들만 받아들여서 upload가 어딜가든 상관없어짐
+videoRouter.get("/:id(\\d+)/edit", edit);
+videoRouter.get("/:id(\\d+)/delete", deleteVideo);
 // parameter는 url안에 argument를 넣는것을 허용해준다.
 export default videoRouter;
