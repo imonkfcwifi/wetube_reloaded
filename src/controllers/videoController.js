dc4745c7942f05b54003b4483b218b5534d4ef31
imonkfcwifi <nohyungkorea@gmail.com>
2022-10-03 22:31:55 +0900

    mixin

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,10 +1,33 @@
-const faker = {
-    username: "faker",
-    loggedIn: false,
-};
+export const homepageVideos = (req, res) => {
+    const videos = [
+        {
+            title: "First Video",
+            rating: 5,
+            comments: 2,
+            createdAt: "2min ago",
+            views: 59,
+            id: 1,
+        },
+        {
+            title: "Second Video",
+            rating: 5,
+            comments: 2,
+            createdAt: "2min ago",
+            views: 59,
+            id: 1,
+        },
+        {
+            title: "Third Video",
+            rating: 5,
+            comments: 2,
+            createdAt: "2min ago",
+            views: 59,
+            id: 1,
+        },
+    ];
+    return res.render("home", { pageTitle: "home", videos });
+}
 
-export const homepageVideos = (req, res) =>
-    res.render("home", { pageTitle: "home", fakerUser: faker });
 export const watch = (req, res) => res.render("watch", { pageTitle: "videos" });
 export const edit = (req, res) => res.render("edit", { pageTitle: "Edit Videos" });
 
