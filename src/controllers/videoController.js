6db335c2a61fe091efbed5044b230b2cdf31a3b5
imonkfcwifi <nohyungkorea@gmail.com>
2022-10-06 21:39:37 +0900

    edit video

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,34 +1,40 @@
-export const homepageVideos = (req, res) => {
-    const videos = [
-        {
-            title: "First Video",
-            rating: 5,
-            comments: 2,
-            createdAt: "2min ago",
-            views: 59,
-            id: 1,
-        },
-        {
-            title: "Second Video",
-            rating: 5,
-            comments: 2,
-            createdAt: "2min ago",
-            views: 59,
-            id: 1,
-        },
-        {
-            title: "Third Video",
-            rating: 5,
-            comments: 2,
-            createdAt: "2min ago",
-            views: 59,
-            id: 1,
-        },
-    ];
-    return res.render("home", { pageTitle: "home", videos });
-}
+let videos = [
+    {
+        title: "First Video",
+        rating: 5,
+        comments: 2,
+        createdAt: "2min ago",
+        views: 59,
+        id: 1,
+    },
+    {
+        title: "Second Video",
+        rating: 5,
+        comments: 2,
+        createdAt: "2min ago",
+        views: 59,
+        id: 2,
+    },
+    {
+        title: "Third Video",
+        rating: 5,
+        comments: 2,
+        createdAt: "2min ago",
+        views: 59,
+        id: 3,
+    },
+];
 
-export const watch = (req, res) => res.render("watch", { pageTitle: "videos" });
+
+export const homepageVideos = (req, res) => { return res.render("home", { pageTitle: "Home", videos }) }
+
+
+export const watch = (req, res) => {
+    const { id } = req.params;
+    const video = videos[id - 1]
+    // const id = req.params.id 와 같음.
+    return res.render("watch", { pageTitle: `Watch ${video.title}`, video });
+}
 export const edit = (req, res) => res.render("edit", { pageTitle: "Edit Videos" });
 
 export const deleteVideo = (req, res) => {
